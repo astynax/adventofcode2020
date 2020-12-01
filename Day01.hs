5d3c69f923c4c6e57c925bbbb0df0e46569d4535
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-12-01 09:18:46 +0300

    init and add a solutions for the day #1

---
+++ Day01.hs
@@ -0,0 +1,24 @@
+main :: IO ()
+main = print =<< (solve . map read . lines) <$> readFile "Day01.input"
+
+solve :: [Int] -> (Int, Int)
+solve rows = (head pairs, head triples)
+  where
+    erows = zip [0..] rows
+    pairs =
+      [ x * y
+      | (ix, x) <- erows
+      , (iy, y) <- erows
+      , ix /= iy
+      , x + y == 2020
+      ]
+    triples =
+      [ x * y * z
+      | (ix, x) <- erows
+      , (iy, y) <- erows
+      , (iz, z) <- erows
+      , ix /= iy
+      , ix /= iz
+      , iy /= iz
+      , x + y + z == 2020
+      ]
