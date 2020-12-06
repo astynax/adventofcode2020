3bc5ed460c4c37aaa1a470f731b3cff831387448
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-12-06 12:56:26 +0300

    add a solution for the day06's problem

---
+++ Day06.hs
@@ -0,0 +1,20 @@
+import Control.Monad (guard)
+import Data.List (unfoldr, foldl')
+import qualified Data.Set as Set
+
+main :: IO ()
+main = print . solve . decode . lines =<< readFile "Day06.input"
+
+decode :: [String] -> [[String]]
+decode = unfoldr $ \l -> do
+  let (x, xs) = break null l
+  guard . not . null $ x
+  pure (x, drop 1 xs)
+
+solve :: [[String]] -> (Int, Int)
+solve groups = (countOnly atLeastOnce, countOnly common)
+  where
+    countOnly f   = sum . map (length . f . map Set.fromList) $ groups
+    atLeastOnce   = foldl' Set.union Set.empty
+    common [x]    = x
+    common (x:xs) = foldl' Set.intersection x xs
