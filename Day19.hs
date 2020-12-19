6e7c3f6c29495107d65c5ce63075f3613db1fd28
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-12-19 19:25:16 +0300

    complete (sort of) a day19's solution

--- Day19.hs
+++ Day19.hs
@@ -1,36 +1,102 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+import Control.Monad.Identity
 import Control.Monad.State
-import Data.List.NonEmpty (NonEmpty(..))
-import qualified Data.List.NonEmpty as NE
 import qualified Data.IntMap.Strict as Map
 import Data.IntMap (IntMap)
+import Data.Kind (Type)
+import Data.List.NonEmpty (NonEmpty(..), (<|))
+import qualified Data.List.NonEmpty as NE
 import Data.Void (Void)
-import Text.Megaparsec
+import Text.Megaparsec hiding (match)
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer (decimal)
 
 type Parser = Parsec Void String
 
-data Ch = A | B deriving Show
-
-type RawRule = Either Ch (NonEmpty (NonEmpty Int))
-
-data Rule
-  = Ch !Ch
-  | Or !(NonEmpty (NonEmpty Rule))
-  deriving Show
-
+data Ch = A | B deriving (Show, Eq)
 type Message = [Ch]
 
-main :: IO ()
-main = print . decode =<< readFile "Day19.input"
+type RawRule = Either Ch (NonEmpty (NonEmpty Int))
+type RawRules = IntMap RawRule
 
-decode :: String -> Maybe (Rule, [Message])
+newtype Simple f = Simple (NonEmpty (NonEmpty f))
+
+data Loop f
+  = LOr (NonEmpty (NonEmpty f))
+  | Loop (NonEmpty f) [f]
+
+data Rule (f :: Type -> Type)
+  = Ch !Ch
+  | Or !(f (Rule f))
+
+deriving instance Show (f (Rule f)) => Show (Rule f)
+
+class Branching t where
+  split :: t a -> NonEmpty (NonEmpty a)
+  wrap :: NonEmpty (NonEmpty a) -> t a
+
+instance Branching Simple where
+  split (Simple x) = x
+  wrap = Simple
+
+instance Branching Loop where
+  split (LOr x)      = x
+  split (Loop xs ys) =
+    case NE.take 5 variants of  -- workaround :(
+      (v: vs) -> v :| vs
+      _       -> error "Impossible!"
+    where
+      variants = (xs <+ ys) <| fmap (\v -> xs <> v <+ ys) (split (Loop xs ys))
+      (a :| as) <+ bs = a :| (as ++ bs)
+  wrap = LOr
+
+main :: IO ()
+main = print . (solve =<<) . decode =<< readFile "Day19.input"
+
+solve :: (RawRules, [Message]) -> Maybe (Int, Int)
+solve (rm, ms) = do
+  r1 <- denormalize @Simple (const id) $ rm
+  r2 <- denormalize patch              $ rm
+  pure
+    ( length $ filter (satisfies r1) ms
+    , length $ filter (satisfies r2) ms
+    )
+
+patch :: Int -> Rule Loop -> Rule Loop
+patch k r = case (k, r) of
+  (8,  Or (LOr ((r1 :| [])   :| []))) -> Or $ Loop (r1 :| []) []
+  (11, Or (LOr ((r1 :| [r2]) :| []))) -> Or $ Loop (r1 :| []) [r2]
+  _                                   -> r
+
+satisfies :: Branching a => Rule a -> Message -> Bool
+satisfies r m = not . null . filter (== (m, [])) $ match r m
+
+match :: Branching a => Rule a -> Message -> [(Message, Message)]
+match _       []     = []
+match (Ch A)  (A:xs) = [([A], xs)]
+match (Ch B)  (B:xs) = [([B], xs)]
+match (Ch _)  _      = []
+match (Or rs) s      = concat . NE.toList . fmap (testAll `flip` s) $ split rs
+
+testAll :: Branching a => NonEmpty (Rule a) -> Message -> [(Message, Message)]
+testAll _         [] = []
+testAll (a :| as) s  = do
+  (m1, s')  <- match a s
+  case as of
+    []     -> pure (m1, s')
+    (x:xs) -> do
+      (ms, s'') <- testAll (x :| xs) s'
+      pure (m1 ++ ms, s'')
+
+decode :: String -> Maybe (IntMap RawRule, [Message])
 decode s = do
   (rs, ms) <- parseMaybe inputP s
   let rm = Map.fromList rs
   guard $ Map.size rm == length rs
-  r <- denormalize rm
-  pure (r, ms)
+  pure (rm, ms)
 
 inputP :: Parser ([(Int, RawRule)], [Message])
 inputP = do
@@ -55,8 +121,11 @@ neSepBy pv ps = do
   (NE.cons v <$> try (ps *> neSepBy pv ps))
     <|> pure (v :| [])
 
-denormalize :: IntMap RawRule -> Maybe Rule
-denormalize rrs = go 0 `evalStateT` Map.empty
+denormalize
+  :: Branching a => (Int -> Rule a -> Rule a)
+  -> IntMap RawRule
+  -> Maybe (Rule a)
+denormalize f rrs = go 0 `evalStateT` Map.empty
   where
     go k = do
       cache <- get
@@ -65,24 +134,8 @@ denormalize rrs = go 0 `evalStateT` Map.empty
         Nothing -> do
           v <- lift $ Map.lookup k rrs
           case v of
-            Left c   -> pure $ Ch c
+            Left c   -> pure . f k $ Ch c
             Right vs -> do
-              r <- Or <$> traverse (traverse go) vs
+              r <- f k . Or . wrap <$> traverse (traverse go) vs
               modify $ Map.insert k r
               pure r
-
-example :: String
-example = unlines
-  [ "0: 4 1 5"
-  , "1: 2 3 | 3 2"
-  , "2: 4 4 | 5 5"
-  , "3: 4 5 | 5 4"
-  , "4: \"a\""
-  , "5: \"b\""
-  , ""
-  , "ababbb"
-  , "bababa"
-  , "abbbab"
-  , "aaabbb"
-  , "aaaabbb"
-  ]
