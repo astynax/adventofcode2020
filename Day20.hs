8b8dacb9862bdf48b2551dd2894108ede4a1692c
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-12-20 19:44:19 +0300

    add first steps of the solution for 20/2

--- Day20.hs
+++ Day20.hs
@@ -1,9 +1,10 @@
-import Control.Monad (guard)
+import Debug.Trace (trace)
 import Data.List (foldl', nub)
 import qualified Data.Map.Strict as Map
 import Data.Map.Strict (Map)
+import Data.Maybe (fromJust)
 import Data.Void (Void)
-import Text.Megaparsec
+import Text.Megaparsec hiding (count)
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer (decimal)
 
@@ -12,8 +13,8 @@ type Parser = Parsec Void String
 data Edged a = Edged !a !([a]) !a deriving (Show, Eq, Ord)
 
 type Tile = Edged (Edged Char)
-type Edge = Edged (Char)
-type Edges = (Edge, Edge, Edge, Edge)
+type Edge = Edged Char
+type Edges a = (Edged a, Edged a, Edged a, Edged a)
 type Input = [(Int, Tile)]
 
 main :: IO ()
@@ -22,14 +23,32 @@ main = print . (solve =<<) . decode =<< readFile "Day20.input"
 solve :: Input -> Maybe (Int, Int)
 solve ts = do
   let em = edgeMap ts
-  let cornerIds = [ i | (i, n) <- Map.toList (idsToNums em), n == 4 ]
-  guard $ length cornerIds == 4
-  pure (product cornerIds, 0)
+  (c, cs) <- borders em
+  let answer1 = product . map fst $ c : cs
+  _ <- build c em
+  pure (answer1, 0)
 
-idsToNums :: Map Edge [(Int, Tile)] -> Map Int Int
-idsToNums em = foldl' f Map.empty [ i | (_, [(i, _)]) <- Map.toList em ]
+build :: (Int, Tile) -> Map Edge [(Int, Tile)] -> Maybe [Tile]
+build (_, e) em = do
+  c <- traverse check (edges e) >>= \case
+    [True, False, False, True] -> pure e
+    [True, True, False, False] -> pure $ mapEdged rotate e
+    [False, True, True, False] -> pure $ mapEdged (rotate . rotate) e
+    [False, False, True, True] -> pure $ mapEdged (rotate . rotate . rotate) e
+    _                          -> Nothing
+  [_, r, _, _] <- pure $ edges c
+  ts <- Map.lookup r em
+  pure $ c : map snd ts
   where
-    f = flip $ Map.insertWith (+) `flip` (1 :: Int)
+    check x = (== 1) . length <$> Map.lookup x em
+
+borders :: Ord a => Map Edge [a] -> Maybe (a, [a])
+borders em = do
+  let xs = Map.toList $ foldl' count Map.empty [ t | (_, [t]) <- Map.toList em ]
+  (c1 : cs@[_, _, _]) <- pure [ t | (t, n) <- xs, n == 4 ]
+  pure (c1, cs)
+  where
+    count = flip $ Map.insertWith (+) `flip` (1 :: Int)
 
 edgeMap :: Input -> Map Edge [(Int, Tile)]
 edgeMap = foldl' step Map.empty
@@ -53,24 +72,35 @@ edgedP p = do
   Just e <- pure (fromList xs) <?> "Have enough elements"
   pure e
 
-edges :: Tile -> Edges
+edges :: Edged (Edged a) -> [Edged a]
 edges (Edged f@(Edged ff _ fl) xs l@(Edged lf _ ll)) =
-  ( f
+  [ f
   , Edged fl (map (\(Edged _ _ x) -> x) xs) ll
-  , reverseEdged $ l
-  , reverseEdged $ Edged ff (map (\(Edged x _ _) -> x) xs) lf
-  )
+  , l
+  , Edged ff (map (\(Edged x _ _) -> x) xs) lf
+  ]
 
 allEdges :: Tile -> [Edge]
-allEdges t = nub $ concatMap fromEdges [es, flipH es, flipV es]
-  where
-    es = edges t
-    fromEdges (u, r, d, l) = [u, r, d, l]
+allEdges t = nub $ concatMap edges
+  [ t
+  , mapEdged reverse t
+  , mapEdged (map reverse) t
+  ]
 
-rotate, flipV, flipH :: Edges -> Edges
-rotate (u, r, d, l) = (l, u, r, d)
-flipV (u, r, d, l) = (d, reverseEdged r, u, reverseEdged l)
-flipH (u, r, d, l) = (reverseEdged u, l, reverseEdged d, r)
+rotate :: [[a]] -> [[a]]
+rotate = reverse . transpose
+
+transpose :: [[a]] -> [[a]]
+transpose vs = case fmap unzip $ traverse uncons vs of
+  Just (a, as) -> a : transpose as
+  Nothing      -> []
+  where
+    uncons []       = Nothing
+    uncons (x : xs) = Just (x, xs)
+
+mapEdged :: ([[a]] -> [[a]]) -> Edged (Edged a) -> Edged (Edged a)
+mapEdged f =
+  fromJust . (fromList =<<) . traverse fromList . f . map toList . toList
 
 toList :: Edged a -> [a]
 toList (Edged x xs y) = x : xs ++ [y]
